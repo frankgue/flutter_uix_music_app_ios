8caa91ccd58d3a627f3a126ca01007773863a634
NEGOD <frankguekeng11@gmail.com>
2020-04-29 17:43:43 +0100

    add image

--- lib/main.dart
+++ lib/main.dart
@@ -30,17 +30,64 @@ class StackBuilder extends StatefulWidget{
 
 }
 
-class _StackBuilderState extends State<StackBuilder> {
+class _StackBuilderState extends State<StackBuilder> with TickerProviderStateMixin{
+
+  AnimationController  paneController;
+  Animation<double> paneAnimation;
+  bool isAnimCompleted = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    paneController = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    paneAnimation = Tween<double>(begin: -250, end: 0.0).animate(paneController);
+
+  }
+
+  animationInit(){
+    if(isAnimCompleted){
+      paneController.reverse();
+    }else{
+      paneController.forward();
+    }
+    isAnimCompleted = !isAnimCompleted;
+  }
+
+
+  Widget stackBody(BuildContext context){
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(image: DecorationImage(image: ExactAssetImage('assets/sale.png'))),
+        ),
+        Positioned(
+          bottom: paneAnimation.value,
+          child: GestureDetector(
+            onTap: () {
+             animationInit();
+            },
+            child: Container(
+              height: MediaQuery.of(context).size.height/2,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.green,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Stack(
-        children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height,
-            color: Colors.red,
-          )
-        ],
+      child: AnimatedBuilder(
+        animation: paneController,
+        builder: (BuildContext context, widget){
+          return stackBody(context);
+        },
       ),
     );
   }
